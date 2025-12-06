4fd5913dcf02fa9b102560cf8530711fad37cb45
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2025-12-07 00:19:35 +0700

    recreated as modules

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <drogon/drogon.h>
-#include <print>
+
+import std;
 
 int main() {
-    //Set HTTP listener address and port
+    // Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", 5555);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //drogon::app().loadConfigFile("../config.yaml");
-    //Run HTTP framework,the method will block in the internal event loop
+    // Load config file
+    // drogon::app().loadConfigFile("../config.json");
+    // drogon::app().loadConfigFile("../config.yaml");
+    // Run HTTP framework,the method will block in the internal event loop
     std::println("App Run!");
     drogon::app().run();
     return 0;
